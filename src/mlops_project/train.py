9048b490a8aa72b2aa62e9837ef51b3d6ea4b805
Emil Bruhn <s246070@dtu.dk>
2026-01-06 15:11:43 +0100

    Adopt mlops_template project structure

---
+++ src/mlops_project/train.py
@@ -0,0 +1,10 @@
+from mlops_project.model import Model
+from mlops_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
