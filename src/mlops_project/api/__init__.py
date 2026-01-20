9d8941bfebee469550998a6dd6e1b98277d5a319
Philippa Bagger <s246103@dtu.dk>
2026-01-20 16:26:56 +0100

    comment out app

--- src/mlops_project/api/__init__.py
+++ src/mlops_project/api/__init__.py
@@ -3,7 +3,7 @@ API module for the MLOps project.
 Contains FastAPI application for model inference.
 """
 
-from mlops_project.api.main import app
+# from mlops_project.api.main import app
 
 
-__all__ = ["app"]
+# __all__ = ["app"]
